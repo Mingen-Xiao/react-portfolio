7cbc786f81c2e46bd879cc97a11bba3817d7f6b8
Lucas Xiao <smy240975417@gmail.com>
2023-12-11 18:47:44 +1030

    keep putting info on the intro page

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,6 @@
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoM from '../../assets/images/logo-m.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
@@ -9,7 +9,7 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
-      <img src={LogoS} alt="logo" />
+      <img src={LogoM} alt="logo" />
       <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
     </Link>
     <nav>
